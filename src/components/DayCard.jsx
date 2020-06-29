888befc6308f8230f41e67c4fc37d6a35a549a28
David Foster <dlfosterii@yahoo.com>
2020-06-29 10:37:36 -0400

    initial commit with basic layout

---
+++ src/components/DayCard.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class DayCard extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Here's the Day Card</h1>
+            </div>
+        )
+    }
+}
