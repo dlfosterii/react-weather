f6c53d04d86057ce6f78574fd334ef775ae45586
David Foster <dlfosterii@yahoo.com>
2020-07-06 10:17:07 -0400

    time displaying properly, app done pre deployment

--- src/components/DayCard.jsx
+++ src/components/DayCard.jsx
@@ -7,7 +7,7 @@ export default function DayCard(props) {
 
         <div className={styles.card}>
             <div className={styles.day}>{new Date((props.forecast.dt + props.city.timezone) * 1000).toLocaleDateString(undefined, { weekday: 'long' })}</div>
-            <div className={styles.time}>{new Date((props.forecast.dt + props.city.timezone) * 1000).toLocaleTimeString('en-US')}</div>
+            <div className={styles.time}>{new Date((props.forecast.dt + props.city.timezone) * 1000).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})}</div>
             <div>
                 <img className={styles.image} src={`http://openweathermap.org/img/wn/${props.forecast.weather[0].icon}@2x.png`} />
                 <div className={styles.desc}>{props.forecast.weather[0].description}</div>
